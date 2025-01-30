2f57c9a12f743d42a7cf4957b2f59e7177a0b7cd
eudeciohasselmann <eudecio@gmail.com>
2025-01-30 17:12:34 -0300

    update FastAPI response and configure Docker networking

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "teste2"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
